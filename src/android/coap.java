352343011310ed74be8dac352c81bc0546aac790
Fengda HUANG <h@phodal.com>
2016-05-07 00:01:46 +0800

    some fix

--- src/android/coap.java
+++ src/android/coap.java
@@ -1,15 +1,9 @@
-package com.phodal.plugin.coap;
+package com.phodal.plugin;
 
 import org.apache.cordova.*;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-
-import org.eclipse.californium.core.CoapClient;
-import org.eclipse.californium.core.CoapResponse;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 
 public class Coap extends CordovaPlugin {
@@ -17,18 +11,13 @@ public class Coap extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("get")) {
-            try {
-                URI uri = new URI("coap://iot.eclipse.org:5683/");
-                CoapClient mCoapClient = new CoapClient(uri);
-                CoapResponse response = mCoapClient.get();
-
-                callbackContext.success(response.getResponseText());
-                return true;
-            } catch (Exception e) {
-
-            }
-
+            callbackContext.success("get");
+            return true;
+        } else if (action.equals("test")) {
+            callbackContext.success("test");
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
 }
--- www/coap.js
+++ www/coap.js
@@ -1,11 +1,10 @@
-/*global cordova*/
+/*global cordova, module*/
 
-var exec = require("cordova/exec");
-
-var Coap = function(){};
-
-Coap.prototype.greet = function (name, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Coap", "get", [name]);
+module.exports = {
+    get: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Coap", "get", [name]);
+    },
+    test: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Coap", "test", [name]);
+    }
 };
-
-module.exports = new Coap();

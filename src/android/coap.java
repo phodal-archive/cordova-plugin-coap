412f75fcbd4477537ec125511fb3241f17647cda
Fengda HUANG <h@phodal.com>
2016-05-07 00:23:14 +0800

    test for coap

--- src/android/coap.java
+++ src/android/coap.java
@@ -5,14 +5,27 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapResponse;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 
 public class Coap extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("get")) {
-            callbackContext.success("get");
-            return true;
+            try {
+                URI uri = new URI("coap://vs0.inf.ethz.ch:5683/");
+                CoapClient mCoapClient = new CoapClient(uri);
+                CoapResponse response = mCoapClient.get();
+                callbackContext.success(response.getResponseText());
+                return true;
+            } catch (URISyntaxException e) {
+                return false;
+            }
         } else if (action.equals("test")) {
             callbackContext.success("test");
             return true;

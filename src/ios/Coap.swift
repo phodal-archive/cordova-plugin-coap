ff965f1c2d7d30676d6f241d1f7529f6df53ae9d
Fengda HUANG <h@phodal.com>
2016-05-07 16:10:25 +0800

    try to use swift

--- src/ios/Coap.swift
+++ src/ios/Coap.swift
@@ -1,8 +1,15 @@
 
 import Foundation
 
- @objc(HWCoapPlugin) class Coap : CDVPlugin {
+@objc(HWCoapPlugin) class Coap : CDVPlugin {
     func get(command: CDVInvokedUrlCommand) {
+
+        let m = SCMessage(code: SCCodeValue(classValue: 0, detailValue: 01)!, type: .Confirmable, payload: "test".dataUsingEncoding(NSUTF8StringEncoding))
+
+        m.addOption(SCOption.UriPath.rawValue, data: "test".dataUsingEncoding(NSUTF8StringEncoding)!)
+
+        let coapClient = SCClient()
+        coapClient.sendCoAPMessage(m, hostName: "coap.me", port: 5683)
 
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Hello World")
         commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)

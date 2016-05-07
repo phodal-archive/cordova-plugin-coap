2d9818cb70485204e02ca57c91f5977150eb693d
Fengda HUANG <h@phodal.com>
2016-05-07 14:40:18 +0800

    upgrade swift

--- src/ios/Coap.swift
+++ src/ios/Coap.swift
@@ -4,12 +4,12 @@ import Foundation
  @objc(HWCoapPlugin) class Coap : CDVPlugin {
     func get(command: CDVInvokedUrlCommand) {
 
-        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Hello World")
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Hello World")
         commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
     }
     func test(command: CDVInvokedUrlCommand) {
 
-        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Hello World")
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Hello World")
         commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
     }
 }
